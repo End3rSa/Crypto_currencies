f239b5d55bbef3038ea784ac7545614f02b4942d
Daria Sumchenko <daria.sumch@gmail.com>
2019-05-02 17:20:41 +0200

    Create recup.twitter.py

---
+++ recup.tweets.rang10/recup.twitter.py
@@ -0,0 +1,14 @@
+#partie twitter
+
+def keystwitter ():
+    api_key = "hsyH5l8Jmqt92rdNYH48R8Q2G"
+    api_secret_key = "5QTiYkp4v0AovzZvyRJ1toPyet7Z1zZyClSzjsnnLKqn3xOajm"
+    access_token = "1123573301568319488-7m3GkF7uDlN8Oy6rDNGiZRrvuKIIoR"
+    access_secret_token = "bqsfhqjydcwa6fPhoyCVWKZhLFoZVuag7WvLPOWCOxRPr"
+
+def twitter.api ():
+api = twitter.Api(consumer_key=api_key,
+                  consumer_secret=api_secret_key,
+                  access_token_key=access_token,
+                  access_token_secret=access_secret_token)
+
